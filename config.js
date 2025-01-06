49284339b946331277320267aaf20240e1f37b4e
toddynnn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2025-01-06 11:58:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    token:  "", // Your bot token
+    channelId: "", //Channel Id you want to send the message
+    nodes: [{
+        host: "",
+        password: "",
+        port: 433,
+        secure: false,
+        name: "Node1"
+      },
+      {
+        host: "",
+        password: "",
+        port: 133,
+        secure: false,
+        name: "Node2"
+      }]
+  };
